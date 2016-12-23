6ab3bb37861f8bf8a3108b59724651524f73848b
Henrique Matheus <mpa.henrique@gmail.com>
2016-12-23 14:21:42 -0200

    LocalStorage Manipulator now on github!!

---
+++ localstorage-manipulator.js
@@ -0,0 +1,28 @@
+function both(method, key, data){
+    window.noLocalStorage = window.noLocalStorage || {};
+    try {
+        localStorage.isAvailable = true;
+        if(method == 'get') {
+            return JSON.parse(localStorage[key]);
+        }
+        localStorage[key] = JSON.stringify(data);
+    } catch(err) {
+        if(method == 'get') {
+            return JSON.parse(window.noLocalStorage[key] || '{}');
+        }
+        window.noLocalStorage[key] = JSON.stringify(data);
+    } 
+}
+
+function saveData(key, data){
+    return both('save', key, data);
+}
+
+function getData(key){
+    return both('get', key);
+}
+
+module.exports = {
+    saveData : saveData,
+    getData  : getData
+}
